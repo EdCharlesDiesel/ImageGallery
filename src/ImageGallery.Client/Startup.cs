ff48e511539cb3be4fadc82485e85353f850334b
Khotso Mokhethi <Mokhetkc@hotmail.com>
2021-02-15 22:50:42 +0200

    Securing User authrntication process
    We learned about sercuring the user authentication process from both the client and the IDP

--- src/ImageGallery.Client/Startup.cs
+++ src/ImageGallery.Client/Startup.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -30,6 +32,35 @@ namespace ImageGallery.Client
                 client.DefaultRequestHeaders.Clear();
                 client.DefaultRequestHeaders.Add(HeaderNames.Accept, "application/json");
             });             
+
+            services.AddAuthentication(options =>
+            {
+                //By setting this value we can sign-in and sign-out of the scheme and read scheme related information. each application with it's own cookie.
+                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                // This referes to when you want to  configure OpenId Scheme.
+                options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+            })
+            .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme) //Enables cookies handler and allows us to use 
+            // cookie based authentication. which all the info will be stored in the cookie.
+
+            //configures the OpenId connect authentication handler
+            .AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
+            {
+                options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.Authority = "https://localhost:50010";
+                options.ClientId = "imagegalleryclient";
+                //Determines the flow used. Theres code, id_token, and id_token token.
+                options.ResponseType = "code";
+                options.UsePkce = true;
+                //options.CallbackPath = new PathString("...")                
+                options.Scope.Add("openid"); //user access token
+                options.Scope.Add("profile"); //user claims tokens
+                options.SaveTokens = true;
+                options.ClientSecret = "secret";
+                options.GetClaimsFromUserInfoEndpoint = true;
+            });
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,6 +83,9 @@ namespace ImageGallery.Client
             app.UseStaticFiles();
 
             app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
